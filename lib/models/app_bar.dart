3fcb828825f1a0dfcde52e24c429066289552519
Fideligo <mrafighalibf@gmail.com>
2025-11-10 12:41:28 +0700

    feat: made an app bar widget model

---
+++ lib/models/app_bar.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+
+  const AppBarWidget({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(
+        title,
+        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+      ),
+      backgroundColor: Color(0xFF07141E),
+      automaticallyImplyLeading: false,
+    );
+  }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}

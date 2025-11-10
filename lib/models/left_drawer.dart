f41d4b33ef1133f0b11ffab2e7458380d61e573a
Fideligo <mrafighalibf@gmail.com>
2025-11-10 12:42:55 +0700

    feat: made a drawer widget model

---
+++ lib/models/left_drawer.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:mamatsportshop_mobile/screens/home_page.dart';
+import 'package:mamatsportshop_mobile/screens/add_product.dart';
+
+class LeftDrawer extends StatelessWidget {
+  const LeftDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Colors.white,
+      child: ListView(
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(color: Color(0xFF07141E)),
+            child: Column(
+              children: [
+                Text(
+                  'Mamat Sportshop',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                Padding(padding: EdgeInsets.all(10)),
+                Text(
+                  "Experience the game like never before.",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.normal,
+                    fontSize: 15,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.home_outlined),
+            title: const Text('Home'),
+            // Bagian redirection ke MyHomePage
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => HomePage()),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.add_box, color: Color(0xFF07141E),),
+            title: const Text('Add Product'),
+            // Bagian redirection ke NewsFormPage
+            onTap: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const AddProductPage()),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
